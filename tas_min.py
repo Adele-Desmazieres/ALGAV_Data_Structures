4aefcfcfe2670c0acb739388a82396060e19f474
Delta-Chat <adele.desmazi@gmail.com>
2023-11-22 15:11:38 +0100

    Tas min Ajout fixed

--- tas_min.py
+++ tas_min.py
@@ -1,4 +1,4 @@
-import int_representation
+import random as rd # temporaire, pour test
 
 class tas_min_interface :
 	
@@ -9,84 +9,109 @@ class tas_min_interface :
 		pass
 
 
+class node :
+	
+	def __init__(self, val) :
+		self.val = val
+		self.gauche = None # node
+		self.droite = None # node
+	
+	def equilibreUnEtage(self, n, isFromRight) :
+		if n.val < self.val :
+			nd, ng = n.droite, n.gauche
+			
+			if isFromRight :
+				n.droite = self
+				n.gauche = self.gauche
+			
+			else :
+				n.droite = self.droite
+				n.gauche = self
+			
+			self.droite = nd
+			self.gauche = ng
+			return n
+		
+		else :
+			if isFromRight :
+				self.droite = n
+			else :
+				self.gauche = n
+			return self
+	
+	def AjoutFin(self, val, chemin) :
+		print(self.toStr())
+		if chemin == [0] :
+			n = node(val)
+			#self.gauche = n
+			return self.equilibreUnEtage(n, 0)
+			
+		elif chemin == [1] :
+			n = node(val)
+			#self.droite = n
+			return self.equilibreUnEtage(n, 1)
+		
+		else : 
+			if chemin[0] == 0 :
+				n = self.gauche.AjoutFin(val, chemin[1:])
+				return self.equilibreUnEtage(n, 0)
+			else :
+				n = self.droite.AjoutFin(val, chemin[1:])
+				return self.equilibreUnEtage(n, 1)
+	
+	def toStr(self) :
+		elt_str = str(self.val)
+		g_str = self.gauche.toStr() if self.gauche else "#"
+		d_str = self.droite.toStr() if self.droite else "#"
+		return "(" + g_str + ", " + elt_str + ", " + d_str + ")"
+
+
 class tas_min_tree(tas_min_interface) :
 	
 	def __init__(self) :
-		self.elt = None
-		self.gauche = None
-		self.droite = None
+		self.root = None
+		self.size = 0
 	
-	def new_tas_min_val(val) :
+	def init_val(val) :
 		t = tas_min_tree()
-		t.elt = val
+		t.root = node(val)
 		return t
 	
 	def AjoutFin(self, val) :
-		if not self.elt :
-			self.elt = val
-			return True
+		if self.root : 
+			# le chemin est la liste des bits de [taille arbre + 1] en binaire, sauf le bit le plus lourd
+			chemin = [int(bit) for bit in bin(self.size + 1)[3:]] # [3:] car on supprime 0b et le first bit
+			#print(chemin)
+			self.root = self.root.AjoutFin(val, chemin)
 		else :
-			if not self.gauche : 
-				self.gauche = tas_min_tree.new_tas_min_val(val)
-				return True
-			elif not self.droite :
-				self.droite = tas_min_tree.new_tas_min_val(val)
-				return True
-			else : 
-				inserted_gauche = self.gauche.AjoutFin(val)
-				if not inserted_gauche :
-					self.droite.AjoutFin(val)
-				return False
+			self.root = node(val)
+		self.size += 1
 	
-	def Ajouttest(self, val) :
-		if not self :
-			return tas_min_tree.new_tas_min_val(val)
-		elif not self.elt :
-			self.elt = val
+	def toStr(self) :
+		if not self.root :
+			return "Empty tree\n"
 		else :
-			if not self.gauche : 
-				self.gauche = tas_min_tree.Ajouttest(self.gauche,val)
-			elif not self.droite :
-				self.droite = tas_min_tree.Ajouttest(self.droite,val)
+			return self.root.toStr()
+
+
+
+def test() :
+	#rd.seed()
+	#x = rd.randrange(100)
+	#xnext = next_pow2(x)
+	#print(x, xnext)
+	#
+	#x = 5
+	#print(bin_list_of_size(5, -1))
+	#
+	#print([tmp(x) for x in range(8)])
 	
-	def AfficherAux(self) :
-		if not self or not self.elt :
-			print("Empty" , end="" , sep="")
-		else :
-			print("( ", end="" , sep="")
-			tas_min_tree.AfficherAux(self.gauche)
-			print(" , " , self.elt , " , " , end="", sep="")
-			tas_min_tree.AfficherAux(self.droite)
-			print(" )" , end="" , sep="")
-		
-	def Afficher(self) :
-		self.AfficherAux()
-		print()
-	
-	def SupprMin(self) -> tas_min_interface :
-		pass
+	t1 = tas_min_tree()
+	for i in range(1, 16, 1) :
+		t1.AjoutFin(i)
+	print()
+	print(t1.toStr())
 
 
-
-
-tas1 = tas_min_tree()
-print(tas1.elt)
-tval = tas_min_tree.new_tas_min_val(3)
-print(tval.elt)
-
-tval.Ajouttest(8)
-tval.Ajouttest(9)
-tval.Afficher()
-
-tree = tas_min_tree()
-tree.AjoutFin(1)
-tree.AjoutFin(2)
-tree.AjoutFin(3)
-tree.AjoutFin(4)
-tree.Afficher()
-class tas_min_array(tas_min_interface) :
-	
-	def __init__(self) :
-		pass
-		
-
+if __name__ == "__main__" :
+	test()
