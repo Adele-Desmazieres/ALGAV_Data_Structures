0bfb1f69f02a2dec1d414a26ed8bd0ec8c504deb
munou <munou47@gmail.com>
2023-11-23 18:29:22 +0100

    SupprMin

--- tas_min.py
+++ tas_min.py
@@ -1,61 +1,102 @@
 import random as rd # temporaire, pour test
 
 class tas_min_interface :
-	
+
 	def SupprMin(self) :
 		pass
-	
+
 	def Ajout(self) :
 		pass
 
 
 class node :
-	
+
 	def __init__(self, val) :
 		self.val = val
 		self.gauche = None # node
 		self.droite = None # node
-	
+
 	def equilibreUnEtage(self, n, isFromRight) :
 		if n.val < self.val :
 			nd, ng = n.droite, n.gauche
-			
+
 			if isFromRight :
 				n.droite = self
 				n.gauche = self.gauche
-			
+
 			else :
 				n.droite = self.droite
 				n.gauche = self
-			
+
 			self.droite = nd
 			self.gauche = ng
 			return n
-		
+
 		else :
 			if isFromRight :
 				self.droite = n
 			else :
 				self.gauche = n
 			return self
-	
+
+	def equilibreDescente(self) :
+		d = self.droite
+		g = self.gauche
+
+# pas d'échange de valeur
+		if (not d and not g) or \
+    			(not d and g.val > self.val) or \
+    			(d and d.val > self.val and g.val > self.val) :
+			return self
+
+# échange à droite
+		elif (d and d.val < self.val and d.val < g.val) :
+			d2, g2 = d.droite, d.gauche
+			d.droite = self
+			d.gauche = g
+			self.droite = d2
+			self.gauche = g2
+			d.droite = d.droite.equilibreDescente()
+			return d
+
+# échange à gauche
+		else :
+			d2, g2 = g.droite, g.gauche
+			g.droite = d
+			g.gauche = self
+			self.droite = d2
+			self.gauche = g2
+			g.gauche = g.gauche.equilibreDescente()
+			return g
+
+	def recuperer(self,chemin) :
+		if chemin == [0] :
+			return self, self.gauche, 0
+		elif chemin == [1] :
+			return self, self.droite, 1
+		else :
+			if chemin[0] == 0 :
+				return self.gauche.recuperer(chemin[1:])
+			else :
+				return self.droite.recuperer(chemin[1:])
+
 	def AjoutNode(self, val, chemin) :
 		if chemin == [0] :
 			n = node(val)
 			return self.equilibreUnEtage(n, 0)
-			
+
 		elif chemin == [1] :
 			n = node(val)
 			return self.equilibreUnEtage(n, 1)
-		
-		else : 
+
+		else :
 			if chemin[0] == 0 :
 				n = self.gauche.AjoutNode(val, chemin[1:])
 				return self.equilibreUnEtage(n, 0)
 			else :
 				n = self.droite.AjoutNode(val, chemin[1:])
 				return self.equilibreUnEtage(n, 1)
-	
+
 	def toStr(self) :
 		elt_str = str(self.val)
 		g_str = self.gauche.toStr() if self.gauche else "#"
@@ -64,18 +105,18 @@ class node :
 
 
 class tas_min_tree(tas_min_interface) :
-	
+
 	def __init__(self) :
 		self.root = None
 		self.size = 0
-	
+
 	def init_val(val) :
 		t = tas_min_tree()
 		t.root = node(val)
 		return t
-	
+
 	def Ajout(self, val) :
-		if self.root : 
+		if self.root :
 			# le chemin est la liste des bits de [taille arbre + 1] en binaire, sauf le bit le plus lourd
 			chemin = [int(bit) for bit in bin(self.size + 1)[3:]] # [3:] car on supprime 0b et le first bit
 			#print(chemin)
@@ -83,13 +124,40 @@ class tas_min_tree(tas_min_interface) :
 		else :
 			self.root = node(val)
 		self.size += 1
-	
+
 	def toStr(self) :
 		if not self.root :
 			return "Empty tree\n"
 		else :
 			return self.root.toStr()
-	
+
+	def SupprMin(self) :
+		if self.size == 0 :
+			return None
+		elif self.size == 1 :
+			self.root = None
+			self.size -= 1
+		else :
+			chemin = [int(bit) for bit in bin(self.size)[3:]]
+			triple = self.root.recuperer(chemin)
+			parent = triple[0]
+			enfant = triple[1]
+			dernier_chemin = triple[2]
+			if dernier_chemin == 0 :
+				enfant.gauche = self.root.gauche
+				enfant.droite = self.root.droite
+				parent.gauche = None
+				self.root = enfant
+			else :
+				enfant.gauche = self.root.gauche
+				enfant.droite = self.root.droite
+				parent.droite = None
+				self.root = enfant
+			self.root = self.root.equilibreDescente()
+			self.size -= 1
+
+
+
 	def AjoutsIteratifs(self, keys) :
 		for k in keys :
 			self.Ajout(k)
@@ -97,21 +165,48 @@ class tas_min_tree(tas_min_interface) :
 
 
 def test() :
-	#rd.seed()
-	#x = rd.randrange(100)
-	#xnext = next_pow2(x)
-	#print(x, xnext)
-	#
-	#x = 5
-	#print(bin_list_of_size(5, -1))
-	#
-	#print([tmp(x) for x in range(8)])
-	
+
 	t1 = tas_min_tree()
-	for i in range(1, 16, 1) :
+	for i in range(1, 9, 1) :
 		t1.Ajout(i)
 	print()
 	print(t1.toStr())
+	chemin1 = [int(bit) for bit in bin(t1.size)[3:]]
+	print()
+	print(chemin1)
+	print()
+	t1.SupprMin()
+	print(t1.toStr())
+	t3 = tas_min_tree()
+	t3.Ajout(2)
+	t3.Ajout(6)
+	t3.Ajout(5)
+	t3.Ajout(10)
+	t3.Ajout(13)
+	t3.Ajout(7)
+	t3.Ajout(8)
+	t3.Ajout(12)
+	t3.Ajout(15)
+	t3.Ajout(14)
+	print()
+	print(t3.toStr())
+	chemin3 = [int(bit) for bit in bin(t3.size)[3:]]
+	print()
+	print(chemin3)
+	print()
+	t3.SupprMin()
+	print(t3.toStr())
+	# t2 = tas_min_tree()
+	# t2.Ajout(2)
+	# print()
+	# print(t2.toStr())
+	#
+	# chemin2 = [int(bit) for bit in bin(t2.size)[3:]]
+	# print(chemin2)
+	# t2.SupprMin()
+	# print()
+	# print(t2.toStr())
+	# t1.SupprMin()
 
 
 if __name__ == "__main__" :
