8b90b608c56e24566414350cde175885faf84541
Delta-Chat <adele.desmazi@gmail.com>
2023-12-04 10:07:26 +0100

    Better graph

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from int_representation import *
 from tas_min import *
 
 INDICES_JEUX_DONNEES = [1, 2, 3, 4, 5]
+#INDICES_JEUX_DONNEES = [5]
 NOMBRE_CLEFS = [1000, 5000, 10000, 20000, 50000, 80000, 120000, 200000]
 DIRECTORY = "cles_alea/"
 
@@ -26,7 +27,7 @@ def moyenne_temps(keys_list, cons_f) :
 
 # [liste_des_listes_de_1000_clefs, liste_des_listes_de_5000_clefs...]
 def graphe_temps(keys_lists_by_size) : 
-    print("Measuring the construction times...")
+    print("Measuring construction times...")
     
     temps_tree_cons = []
     temps_tree_ajout = []
@@ -39,23 +40,26 @@ def graphe_temps(keys_lists_by_size) :
         temps_array_cons.append(moyenne_temps(keys_list, tas_min_array.Construction))
         temps_array_ajout.append(moyenne_temps(keys_list, tas_min_array.AjoutsIteratifsStatic))
 
+    print("Making plots...")
     fig, axs = plt.subplots(1, 2)
     fig.suptitle("Temps de création d'un tas min en fonction de sa taille")
     
     xval = [n/1000 for n in NOMBRE_CLEFS]
     
-    axs[0].plot(xval, temps_tree_cons, label="Construction")
-    axs[0].plot(xval, temps_tree_ajout, label="AjoutsIteratifs")
+    axs[0].plot(xval, temps_tree_cons, label="Construction", marker='X')
+    axs[0].plot(xval, temps_tree_ajout, label="AjoutsIteratifs", marker='X')
     axs[0].legend()
-    axs[0].set(xlabel="Taille du tas (en milliers de noeuds)", ylabel="Temps de création (en secondes)")
+    axs[0].set(xlabel="taille du tas (en milliers de noeuds)", ylabel="temps de création (en secondes)")
     axs[0].set_title("Tas min par arbre")
+    #axs[0].set_xticks(xval, minor=False)
     axs[0].grid()
 
-    axs[1].plot(xval, temps_array_cons, label="Construction")
-    axs[1].plot(xval, temps_array_ajout, label="AjoutsIteratifs")
+    axs[1].plot(xval, temps_array_cons, label="Construction", marker='X')
+    axs[1].plot(xval, temps_array_ajout, label="AjoutsIteratifs", marker='X')
     axs[1].legend()
-    axs[1].set(xlabel="Taille du tas (en milliers de noeuds)")
+    axs[1].set(xlabel="taille du tas (en milliers de noeuds)")
     axs[1].set_title("Tas min par tableau")
+    #axs[1].set_xticks(xval, minor=False)
     axs[1].grid()
     
     plt.show()
@@ -75,7 +79,7 @@ def process_keys(filename) :
 
 def process_all_keys() :
     keys_lists_by_size = [[None for x in range(len(INDICES_JEUX_DONNEES))] for y in range(len(NOMBRE_CLEFS))]
-    print("Processing the files...")
+    print("Processing files...")
     #print(len(keys_lists_by_size))
     #print(len(keys_lists_by_size[0]))
     #print(len(keys_lists_by_size[0][0]))
