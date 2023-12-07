62e88ffa6d9d030c5aeb764c3f4183ed7d888bc9
Delta-Chat <adele.desmazi@gmail.com>
2023-12-07 15:43:43 +0100

    New structure of binomial queue

--- file_binomiale.py
+++ file_binomiale.py
@@ -3,43 +3,100 @@ class Node:
     
     def __init__(self, key):
         self.val = key
-        self.childs = []
+        self.child = None
+        self.next_bro = None
+        self.prev_bro = None
+        
+        #self.childs = []
     
     def __str__(self) :
-        ret = str(self.val) + " pere de [ "
-        other_nodes = ""
-        for child in self.childs :
-            ret += str(child.val) + " "
-            other_nodes += child.__str__()
-        ret = ret + "]\n" + other_nodes
-        return ret
+        ret = str(self.val)
+        others = ""
+        
+        if self.child:
+            ret += ", pere de " + str(self.child.val)
+            others += self.child.__str__()
+        
+        if self.next_bro:
+            ret += ", frere de " + str(self.next_bro.val)
+            others += self.next_bro.__str__()
+        
+        return ret + "\n" + others
 
-
-class HeapMin:
-    
-    def __init__(self, key):
-        self.root = Node(key)
-        self.size = 1
-    
-    def __str__(self) :
-        return "HeapMin\n" + self.root.__str__()
-    
-    def union(self, hm2):
-        if self.size != hm2.size:
-            raise IllegalArgumentException("Union of heaps of different sizes impossible.")
-        if self.root.val > hm2.root.val:
-            return hm2.union(self)
+    def setBigBrotherOf(self, n):
+        self.next_bro = n
+        if n is not None:
+            n.prev_bro = self
+        
+    def union(self, n):
+        if n is None: return self
+        
+        if self.val > n.val:
+            return n.union(self)
         else:
-            self.root.childs = [hm2.root] + self.root.childs
-            self.size += hm2.size
+            n.setBigBrotherOf(self.child)
+            self.child = n
             return self
-
+    
+    def getBrothers(self):
+        l = []
+        if self.next_bro:
+            l = self.next_bro.getBrothers()
+        return l.append(self)
+    
+    def degre(self):
+        return len(self.child.getBrothers())
+    
+    def sortByDeg(self):
+        root = self
+        curr = self
+        while curr not None:
+            pass
+            # TODO
+        
+        
+        return root
+    
+    def decapite(self):
+        return BinomialQueue.initTournoisDecapite(self)
+    
+    def file(self):
+        return BinomialQueue.initTournois(self)
+    
 
 class BinomialQueue:
     
+    # la file binomiale est une liste doublement chaînée de tournois
+    # du tournois le plus petit au tournois le plus grand
     def __init__(self):
-        self.heaps = [] # tableau, ou head d'une liked list de heap min ?
-        self.size = 0
+        self.head = None # la racine du tournois le plus petit
+        self.min_key_node = None # la racine du tournois dont la clef est la plus petite
+    
+    def initTournoisDecapite(node):
+        racine = node.sortByDeg()
+        self.head = racine
+        self.min_key_node = min(node.getBrothers())
+        return self
+    
+    def initTournois(node):
+        self.head = node
+        self.min_key_node = node
+        return self
+    
+    def __str__(self):
+        if self.head:
+            return "File binomiale\n" + self.head.__str__()
+        else:
+            return "File binomiale vide\n"
+    
+    def isEmpty(self):
+        return (self.head is None)
+    
+    def minDeg(self):
+        return self.minroot
+    
+    def reste(self):
+        
     
     def SupprMin(self):
         pass
@@ -58,21 +115,25 @@ class BinomialQueue:
 
 
 
-
 def test():
     
-    h1 = HeapMin(4)
-    h2 = HeapMin(2)
-    hu1 = h1.union(h2)
-    print(hu1)
-
-    h3 = HeapMin(5)
-    h4 = HeapMin(3)
-    hu2 = h4.union(h3)
-    print(hu2)
+    n7 = Node(7)
+    n12 = Node(12)
+    n8 = Node(8)
+    n13 = Node(13)
     
-    h = hu2.union(hu1)    
-    print(h)
+    #n7.child = n12
+    #n12.setBigBrotherOf(n8)
+    #n12.child = n13
+    n7 = n7.union(n8)
+    n12 = n12.union(n13)
+    n7 = n12.union(n7)
+    print(n7)
+
+    #b = BinomialQueue()
+    
+    #print(n7)
+    
     
 
 
