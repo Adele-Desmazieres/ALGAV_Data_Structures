ee9a5f9132eaec3e5be8728ee92c4cb5b86cb18b
Delta-Chat <adele.desmazi@gmail.com>
2023-12-17 16:55:35 +0100

    Bigger plots

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from int_representation import *
 from tas_min import *
 
 INDICES_JEUX_DONNEES = [1, 2, 3, 4, 5]
-NOMBRE_CLEFS = [1000, 5000, 10000, 20000, 50000, 80000, 120000, 200000]
+NOMBRE_CLEFS = [1000, 5000, 10000, 20000, 50000, 80000, 120000, 200000, 400000, 1000000]
 DIRECTORY = "cles_alea/"
 
 def mesure_temps_union(keys1, keys2) :
@@ -70,9 +70,6 @@ def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_arra
 
     plt.show()
     plt.close()
-    
-    
-    
 
 
 def process_keys(filename) :
@@ -94,6 +91,7 @@ def process_all_keys() :
     temps_array_ajout = [0] * len(NOMBRE_CLEFS)
     temps_tree_union = [0] * len(NOMBRE_CLEFS)
     temps_array_union = [0] * len(NOMBRE_CLEFS)
+    nombre_de_jeux = [0] * len(NOMBRE_CLEFS)
 
     print("Processing files...")
     
@@ -104,28 +102,45 @@ def process_all_keys() :
             jeu_indice = INDICES_JEUX_DONNEES[j]
             
             filename = DIRECTORY + "jeu_" + str(jeu_indice) + "_nb_cles_" + str(keys_number) + ".txt"
-            print("\t" + filename)
-            keys = process_keys(filename)
-            t1 = mesure_temps(keys, tas_min_tree.Construction)
-            t2 = mesure_temps(keys, tas_min_tree.AjoutsIteratifsStatic)
-            t3 = mesure_temps(keys, tas_min_array.Construction)
-            t4 = mesure_temps(keys, tas_min_array.AjoutsIteratifsStatic)
-            mid = keys_number//2
-            t5, t6 = mesure_temps_union(keys[:mid+1], keys[mid+1:])
             
-            temps_tree_cons[i] += t1
-            temps_tree_ajout[i] += t2
-            temps_array_cons[i] += t3
-            temps_array_ajout[i] += t4
-            temps_tree_union[i] += t5
-            temps_array_union[i] += t6
+            try:
+                keys = process_keys(filename)
+                print("\t" + filename)
+                
+                #t1 = mesure_temps(keys, tas_min_tree.Construction)
+                #t2 = mesure_temps(keys, tas_min_tree.AjoutsIteratifsStatic)
+                t1 = 0
+                t2 = 0
+                t3 = mesure_temps(keys, tas_min_array.Construction)
+                t4 = mesure_temps(keys, tas_min_array.AjoutsIteratifsStatic)
+                mid = keys_number//2
+                #t5, t6 = mesure_temps_union(keys[:mid+1], keys[mid+1:])
+                t5, t6 = 0, 0
+                
+                temps_tree_cons[i] += t1
+                temps_tree_ajout[i] += t2
+                temps_array_cons[i] += t3
+                temps_array_ajout[i] += t4
+                temps_tree_union[i] += t5
+                temps_array_union[i] += t6
+                
+                nombre_de_jeux[i] += 1
+            
+            except FileNotFoundError:
+                pass
 
-    temps_tree_cons = [x/len(temps_tree_cons) for x in temps_tree_cons]
-    temps_tree_ajout = [x/len(temps_tree_ajout) for x in temps_tree_ajout]
-    temps_array_cons = [x/len(temps_array_cons) for x in temps_array_cons]
-    temps_array_ajout = [x/len(temps_array_ajout) for x in temps_array_ajout]
-    temps_tree_union = [x/len(temps_tree_union) for x in temps_tree_union]
-    temps_array_union = [x/len(temps_array_union) for x in temps_array_union]
+    temps_tree_cons = [temps_tree_cons[i]/nombre_de_jeux[i] for i in range(len(temps_tree_cons))]
+    temps_tree_ajout = [temps_tree_ajout[i]/nombre_de_jeux[i] for i in range(len(temps_tree_ajout))]
+    temps_array_cons = [temps_array_cons[i]/nombre_de_jeux[i] for i in range(len(temps_array_cons))]
+    temps_array_ajout = [temps_array_ajout[i]/nombre_de_jeux[i] for i in range(len(temps_array_ajout))]
+    temps_tree_union = [temps_tree_union[i]/nombre_de_jeux[i] for i in range(len(temps_tree_union))]
+    temps_array_union = [temps_array_union[i]/nombre_de_jeux[i] for i in range(len(temps_array_union))]
+    
+    #temps_tree_ajout = [x/len(temps_tree_ajout) for x in temps_tree_ajout]
+    #temps_array_cons = [x/len(temps_array_cons) for x in temps_array_cons]
+    #temps_array_ajout = [x/len(temps_array_ajout) for x in temps_array_ajout]
+    #temps_tree_union = [x/len(temps_tree_union) for x in temps_tree_union]
+    #temps_array_union = [x/len(temps_array_union) for x in temps_array_union]
     
     return (temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_array_ajout, temps_tree_union, temps_array_union)
 
