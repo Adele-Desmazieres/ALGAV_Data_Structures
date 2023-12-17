993fe4f8e46570d8d686d95179878c4da604bb83
Delta-Chat <adele.desmazi@gmail.com>
2023-12-17 19:31:25 +0100

    Graphe des files

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import time
 
 from int_representation import *
 from tas_min import *
+from file_binomiale import * 
 
-INDICES_JEUX_DONNEES = [1, 2, 3, 4, 5]
-NOMBRE_CLEFS = [1000, 5000, 10000, 20000, 50000, 80000, 120000, 200000, 400000, 1000000]
+INDICES_JEUX_DONNEES = [1, 2]#, 3, 4, 5]
+NOMBRE_CLEFS = [1000, 5000, 10000, 20000, 50000, 80000, 120000, 200000]#, 400000, 1000000]
 DIRECTORY = "cles_alea/"
 
 def mesure_temps_union(keys1, keys2) :
     ttree1 = tas_min_tree.Construction(keys1)
     ttree2 = tas_min_tree.Construction(keys2)
+    
     tarr1 = tas_min_array.AjoutsIteratifsStatic(keys1)
     tarr2 = tas_min_array.AjoutsIteratifsStatic(keys2)
+    
+    tfile1 = BinomialQueue.Construction(keys1)
+    tfile2 = BinomialQueue.Construction(keys2)
     
     start = time.time()
     tas_min_tree.Union(ttree1, ttree2)
@@ -22,7 +27,11 @@ def mesure_temps_union(keys1, keys2) :
     tas_min_array.Union(tarr1, tarr2)
     arr_time = time.time() - start
     
-    return tree_time, arr_time
+    start = time.time()
+    BinomialQueue.UnionFile(tfile1, tfile2)
+    file_time = time.time() - start
+    
+    return tree_time, arr_time, file_time
 
 def mesure_temps(keys, cons_f) :
     start = time.time()
@@ -31,11 +40,11 @@ def mesure_temps(keys, cons_f) :
     return ret
 
 
-def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_array_ajout, temps_tree_union, temps_array_union) : 
+def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_array_ajout, temps_tree_union, temps_array_union, temps_file_cons, temps_file_union) : 
     print("Making plots...")
     
     # FIRST GRAPH
-    fig, axs = plt.subplots(1, 2)
+    fig, axs = plt.subplots(1, 3)
     fig.suptitle("Temps de création d'un tas min en fonction de sa taille")
     
     xval = [n/1000 for n in NOMBRE_CLEFS]
@@ -45,7 +54,6 @@ def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_arra
     axs[0].legend()
     axs[0].set(xlabel="taille du tas (en milliers de noeuds)", ylabel="temps de création (en secondes)")
     axs[0].set_title("Tas min par arbre")
-    #axs[0].set_xticks(xval, minor=False)
     axs[0].grid()
 
     axs[1].plot(xval, temps_array_cons, label="Construction", marker='X')
@@ -53,8 +61,13 @@ def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_arra
     axs[1].legend()
     axs[1].set(xlabel="taille du tas (en milliers de noeuds)")
     axs[1].set_title("Tas min par tableau")
-    #axs[1].set_xticks(xval, minor=False)
     axs[1].grid()
+
+    axs[2].plot(xval, temps_file_cons, label="Construction", marker='X')
+    axs[2].legend()
+    axs[2].set(xlabel="taille de la file (en milliers de noeuds)")
+    axs[2].set_title("File binomiale")
+    axs[2].grid()
     
     plt.show()
     #plt.close()
@@ -62,6 +75,7 @@ def graphe_temps(temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_arra
     # SECOND GRAPH
     plt.plot(xval, temps_tree_union, label="Union par arbre", marker='X')
     plt.plot(xval, temps_array_union, label="Union par tableau", marker='X')
+    plt.plot(xval, temps_file_union, label="Union de files", marker='X')
     plt.legend()
     plt.xlabel("taille du tas résultant (en milliers de noeuds)")
     plt.ylabel("temps (en secondes)")
@@ -91,6 +105,8 @@ def process_all_keys() :
     temps_array_ajout = [0] * len(NOMBRE_CLEFS)
     temps_tree_union = [0] * len(NOMBRE_CLEFS)
     temps_array_union = [0] * len(NOMBRE_CLEFS)
+    temps_file_cons = [0] * len(NOMBRE_CLEFS)
+    temps_file_union = [0] * len(NOMBRE_CLEFS)
     nombre_de_jeux = [0] * len(NOMBRE_CLEFS)
 
     print("Processing files...")
@@ -107,15 +123,20 @@ def process_all_keys() :
                 keys = process_keys(filename)
                 print("\t" + filename)
                 
-                #t1 = mesure_temps(keys, tas_min_tree.Construction)
-                #t2 = mesure_temps(keys, tas_min_tree.AjoutsIteratifsStatic)
-                t1 = 0
-                t2 = 0
+                t1 = mesure_temps(keys, tas_min_tree.Construction)
+                t2 = mesure_temps(keys, tas_min_tree.AjoutsIteratifsStatic)
+                #t1 = 0
+                #t2 = 0
+                
                 t3 = mesure_temps(keys, tas_min_array.Construction)
                 t4 = mesure_temps(keys, tas_min_array.AjoutsIteratifsStatic)
+                
                 mid = keys_number//2
-                #t5, t6 = mesure_temps_union(keys[:mid+1], keys[mid+1:])
-                t5, t6 = 0, 0
+                t5, t6, t7 = mesure_temps_union(keys[:mid+1], keys[mid+1:])
+                #t5, t6 = 0, 0
+                
+                t8 = mesure_temps(keys, BinomialQueue.Construction)
+                
                 
                 temps_tree_cons[i] += t1
                 temps_tree_ajout[i] += t2
@@ -123,6 +144,8 @@ def process_all_keys() :
                 temps_array_ajout[i] += t4
                 temps_tree_union[i] += t5
                 temps_array_union[i] += t6
+                temps_file_union[i] += t7
+                temps_file_cons[i] += t8
                 
                 nombre_de_jeux[i] += 1
             
@@ -135,6 +158,8 @@ def process_all_keys() :
     temps_array_ajout = [temps_array_ajout[i]/nombre_de_jeux[i] for i in range(len(temps_array_ajout))]
     temps_tree_union = [temps_tree_union[i]/nombre_de_jeux[i] for i in range(len(temps_tree_union))]
     temps_array_union = [temps_array_union[i]/nombre_de_jeux[i] for i in range(len(temps_array_union))]
+    temps_file_cons = [temps_file_cons[i]/nombre_de_jeux[i] for i in range(len(temps_file_cons))]
+    temps_file_union = [temps_file_union[i]/nombre_de_jeux[i] for i in range(len(temps_file_union))]
     
     #temps_tree_ajout = [x/len(temps_tree_ajout) for x in temps_tree_ajout]
     #temps_array_cons = [x/len(temps_array_cons) for x in temps_array_cons]
@@ -142,12 +167,13 @@ def process_all_keys() :
     #temps_tree_union = [x/len(temps_tree_union) for x in temps_tree_union]
     #temps_array_union = [x/len(temps_array_union) for x in temps_array_union]
     
-    return (temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_array_ajout, temps_tree_union, temps_array_union)
+    return (temps_tree_cons, temps_tree_ajout, temps_array_cons, temps_array_ajout, \
+        temps_tree_union, temps_array_union, temps_file_cons, temps_file_union)
 
 
 def main() :
-    t1, t2, t3, t4, t5, t6 = process_all_keys()
-    graphe_temps(t1, t2, t3, t4, t5, t6)
+    t1, t2, t3, t4, t5, t6, t7, t8 = process_all_keys()
+    graphe_temps(t1, t2, t3, t4, t5, t6, t7, t8)
     print("Done.")
 
 
